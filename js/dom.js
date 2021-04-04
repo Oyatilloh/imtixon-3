69542a8f8aa8ae5c0b8287238c58b0ff5bda4f54
Oyatilloh <77911448+Oyatilloh@users.noreply.github.com>
2021-04-04 15:47:36 +0500

    Initial commit

---
+++ js/dom.js
@@ -0,0 +1,20 @@
+(function() {
+  const select = document.querySelectorAll('select');
+  const options = Array.from(select[0].options);
+  const input = document.querySelector('input');
+
+  function findMatches (search, options) {
+    return options.filter(option => {
+      const regex = new RegExp(search, 'gi');
+      return option.text.match(regex);
+    });
+  }
+
+  function filterOptions () {
+    options.forEach(option => { 
+      option.remove();
+      option.selected = false;
+    });
+    const matchArray = findMatches(this.value, options);
+    select[0].append(...matchArray);
+  }
